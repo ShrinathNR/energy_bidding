0e9dbed359b5b65be02c218c984cf754af243cfe
ShrinathNR <nr.shrinath@gmail.com>
2023-04-04 18:11:04 +0530

    edited bidding page

---
+++ Arduino Codes/producer/producer_meter.ino
@@ -0,0 +1,44 @@
+/*  @file       producer_meter.ino
+
+*/
+
+/*
+*   A 5V relays NC terminals are connected between the Producer's PV Emulator and the Battery Bank.
+*   Under Normal Operation, the excess energy generated is stored in the batteries and
+*   a DC meter in between records the enrgy transfer.
+*
+*   This Relay recieves commands from the Producer's Network Server Backend i.e. Producer.js
+*   through serial port.
+*
+*   Upon recieving a value '1' from the server, this relay changes its state
+*   and the battery stops charging. Original condition is restored upon recieving a value '2'.
+*
+*   We have set the default value to be Low, i.e. the PV emulator output charges the battery at
+*   the start.
+*/
+
+int value = 2;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(PD2,OUTPUT);
+  digitalWrite(PD2,LOW); // Default Condition
+}
+
+void loop() 
+{
+  value = Serial.parseInt();  // Read the serial data
+
+  if(value == 1)
+  {
+    // Toggle connection to stop charging the batteries
+    digitalWrite(PD2,HIGH);
+  } 
+
+  if(value == 2)
+  {
+    // Go back to default state
+    digitalWrite(PD2,LOW);
+  }
+}
